235eeb9455f2bf7129c7d101c7d4922566694aa3
George Porter <gmporter@cs.ucsd.edu>
2018-11-17 13:04:13 -0800

    Starter code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hw8-starter-code
-Homework 8 starter code
+# CSE 224 Homework 8
+
+Basic starter code for Homework 8
+
+raftnode.py should implement the code for a single RAFT node.
+
+You will likely want to implement some type of client that can
+query your RAFT nodes to determine which one of them, if any,
+is a leader.
---
+++ raftnode.py
@@ -0,0 +1,39 @@
+import rpyc
+import sys
+
+'''
+A RAFT RPC server class.
+
+Please keep the signature of the is_leader() method unchanged (though
+implement the body of that function correctly.  You will need to add
+other methods to implement ONLY the leader election part of the RAFT
+protocol.
+'''
+class RaftNode(rpyc.Service):
+	
+
+	"""
+        Initialize the class using the config file provided and also initialize
+        any datastructures you may need.
+	"""
+	def __init__(self, config):
+		pass
+
+	'''
+        x = is_leader(): returns True or False, depending on whether
+        this node is a leader
+
+        As per rpyc syntax, adding the prefix 'exposed_' will expose this
+        method as an RPC call
+
+        CHANGE THIS METHOD TO RETURN THE APPROPRIATE RESPONSE
+	'''
+	def exposed_is_leader(self):
+		return False
+
+
+if __name__ == '__main__':
+	from rpyc.utils.server import ThreadPoolServer
+	server = ThreadPoolServer(RaftNode(sys.argv[1]), port = 6000)
+	server.start()
+
